eee6719bb32d0f5300d8f3259b130ab1b113a02d
moth4017 <mark4017@hotmail.com>
2022-08-12 20:42:39 +1200

    Update drybox.h

--- Firmware/DryBox  Firmware addon_ beta/DryBox_valkyrie/drybox.h
+++ Firmware/DryBox  Firmware addon_ beta/DryBox_valkyrie/drybox.h
@@ -1,8 +1,8 @@
 // wifi options
-const char* ssid = "pronto";
-const char* password = "fcfcfcfcfc";
+const char* ssid = "enter ssid";
+const char* password = "enter password";
 //#define STANDALONE 1             // delete line if you are running DWC
-#define DWC_NAME "3030.local"              // name/IP of DWC 
+#define DWC_NAME "valkyrie.local"              // name/IP of DWC 
 #define FLASH_LED 4              //LED to blink on wifi connection
 #define DRYBOX_NAME "drybox"  //local name of esp32
 int reportTime = 800;            // report weight to DWC interval
