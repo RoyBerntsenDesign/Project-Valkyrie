6afcb24fbe750a1d43e156883966f603de0691bb
Roy Berntsen <roy@pro3d.no>
2022-09-13 12:52:40 +0200

    IP adress added

--- Firmware/drybox/dbx platform IO/src/drybox.h
+++ Firmware/drybox/dbx platform IO/src/drybox.h
@@ -1,8 +1,7 @@
 // wifi options
-const char* ssid = "";
-const char* password = "";
-//#define STANDALONE 1           // delete line if you are running DWC
-#define DWC_NAME ""              // name/IP of DWC 
+const char* ssid = "PRO3D";
+const char* password = "79496261";
+#define DWC_NAME "192.168.101.233"              // name/IP of DWC 
 #define FLASH_LED 4              //LED to blink on wifi connection
 #define DRYBOX_NAME "drybox"     //local name of esp32
 int reportTime = 500;            // report weight to DWC interval
