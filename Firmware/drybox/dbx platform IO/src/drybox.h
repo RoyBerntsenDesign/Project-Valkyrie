bf30e2af3bb4963fdc2add24e9ac5cd9424f6a95
Roy Berntsen <roy@pro3d.no>
2022-09-14 11:21:37 +0200

    BOM update
    Changed the drybox describtion to sunon

--- Firmware/drybox/dbx platform IO/src/drybox.h
+++ Firmware/drybox/dbx platform IO/src/drybox.h
@@ -1,7 +1,7 @@
 // wifi options
-const char* ssid = "PRO3D";
+const char* ssid = "FA-PRO3D";
 const char* password = "79496261";
-#define DWC_NAME "192.168.101.233"              // name/IP of DWC 
+#define DWC_NAME "vd36hc.local"              // name/IP of DWC 
 #define FLASH_LED 4              //LED to blink on wifi connection
 #define DRYBOX_NAME "drybox"     //local name of esp32
 int reportTime = 500;            // report weight to DWC interval
